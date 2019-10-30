fe51bdaec788e5ae184e18e71008fc4bd02966cd
teramotodaiki <i101323@gmail.com>
2019-10-30 14:57:34 +0900

    fix: footer open/close

--- src/footer.tsx
+++ src/footer.tsx
@@ -4,28 +4,55 @@ import { Balloon } from './balloon';
 import { IconButton } from './button';
 import flex from './css/flex.scss';
 import footer from './css/footer.scss';
+import { Transition } from './transition';
 
 export interface FooterProps {
   onItemClick: () => void;
 }
 
 export function Footer({ onItemClick }: FooterProps) {
+  const [opened, setOpened] = React.useState(false);
+  const close = React.useCallback(() => {
+    setOpened(false);
+  }, []);
+
   return (
-    <div className={classNames(footer.container, flex.horizontal)}>
-      <IconButton name="search" className={footer.search}>
-        <Balloon icon="info" delay={5000}>
-          Add charactor to the game
-        </Balloon>
-      </IconButton>
-      <FooterItem onClick={onItemClick} />
-      <FooterItem onClick={onItemClick} />
-      <div className={footer.divider}></div>
-      <FooterItem onClick={onItemClick} />
-      <FooterItem onClick={onItemClick} />
-      <FooterItem onClick={onItemClick} />
-      <FooterItem onClick={onItemClick} />
-      <FooterItem onClick={onItemClick} />
-      <FooterItem onClick={onItemClick} />
+    <div className={classNames(footer.container)}>
+      {opened ? <div className={footer.backdrop} onClick={close}></div> : null}
+      <Transition in={opened} className={footer.pane} exiting={footer.hidden}>
+        <IconButton name="close" className={footer.close} onClick={close} />
+        <FooterPane />
+      </Transition>
+      <div className={classNames(footer.bar, flex.horizontal)}>
+        <IconButton
+          name="search"
+          className={footer.search}
+          onClick={() => setOpened(!opened)}
+        >
+          <Balloon icon="info" delay={5000}>
+            Add charactor to the game
+          </Balloon>
+        </IconButton>
+        {opened ? (
+          <input
+            type="text"
+            autoFocus
+            placeholder="Search monsters, people and items"
+          />
+        ) : (
+          <>
+            <FooterItem onClick={onItemClick} />
+            <FooterItem onClick={onItemClick} />
+            <div className={footer.divider}></div>
+            <FooterItem onClick={onItemClick} />
+            <FooterItem onClick={onItemClick} />
+            <FooterItem onClick={onItemClick} />
+            <FooterItem onClick={onItemClick} />
+            <FooterItem onClick={onItemClick} />
+            <FooterItem onClick={onItemClick} />
+          </>
+        )}
+      </div>
     </div>
   );
 }
@@ -40,4 +67,10 @@ export function FooterItem({ onClick }: FooterItemProps) {
       <div style={{ backgroundColor: 'blue' }}></div>
     </button>
   );
+}
+
+export interface FooterPaneProps {}
+
+export function FooterPane({  }: FooterPaneProps) {
+  return <></>;
 }
