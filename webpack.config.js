6cd35eb1f045fe91c0b87f3839b298b518095f29
teramotodaiki <i101323@gmail.com>
2019-11-16 15:06:48 +0900

    Use webpack to gen declaration

---
+++ webpack.config.js
@@ -0,0 +1,46 @@
+const path = require('path');
+
+module.exports = {
+  mode: 'production',
+  entry: {
+    index: './src/index.ts'
+  },
+  output: {
+    path: path.resolve(__dirname, 'lib'),
+    filename: '[name].js'
+  },
+  resolve: {
+    extensions: ['.js', '.ts', '.tsx']
+  },
+  module: {
+    rules: [
+      {
+        test: /\.ts(x?)$/i,
+        exclude: /node_modules/,
+        use: [
+          {
+            loader: 'ts-loader'
+          }
+        ]
+      },
+      {
+        test: /\.(s?)css$/i,
+        use: [
+          { loader: 'style-loader' },
+          {
+            loader: 'css-loader',
+            options: {
+              modules: true
+            }
+          },
+          { loader: 'sass-loader' }
+        ]
+      }
+    ]
+  },
+  externals: {
+    react: 'React',
+    'react-dom': 'ReactDOM'
+  },
+  devtool: 'source-map'
+};
