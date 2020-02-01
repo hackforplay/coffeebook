76cf410565c94c04c272441f80d925d64bbd7a98
teramotodaiki <i101323@gmail.com>
2020-02-01 18:36:18 +0900

    fix: detect difference of small/capital case

---
+++ src/Card.tsx
@@ -0,0 +1,26 @@
+import classNames from 'classnames';
+import * as React from 'react';
+import cards from './styles/cards.scss';
+
+export interface PaperProps {
+  elevated: boolean;
+  children: React.ReactNode;
+  className?: string;
+}
+
+export function Card({ elevated, children, className }: PaperProps) {
+  return (
+    <div
+      className={classNames(
+        elevated ? cards.elevated : cards.outlined,
+        className
+      )}
+    >
+      {children}
+    </div>
+  );
+}
+
+export function CardDivider() {
+  return <div className={cards.divider}></div>;
+}
---
+++ src/Footer.tsx
@@ -0,0 +1,87 @@
+import classNames from 'classnames';
+import * as React from 'react';
+import { useDispatch } from 'react-redux';
+import { Balloon } from './Balloon';
+import { IconButton } from './Button';
+import flex from './styles/flex.scss';
+import footer from './styles/footer.scss';
+import { dummyAssets } from './dummy-assets';
+import { FooterPane } from './FooterPane';
+import { actions, EditorMode } from './store';
+import { Transition } from './Transition';
+
+export interface FooterProps {}
+
+export function Footer({  }: FooterProps) {
+  const [opened, setOpened] = React.useState(false);
+  const close = React.useCallback(() => {
+    setOpened(false);
+  }, []);
+  const [assets] = React.useState(dummyAssets);
+  const [search, setSearch] = React.useState('');
+
+  return (
+    <>
+      <Transition
+        in={opened}
+        className={classNames(footer.container, flex.vertical)}
+        exiting={footer.hidden}
+      >
+        <FooterPane assets={assets} onRequestClose={close} search={search} />
+      </Transition>
+      <div
+        className={classNames(
+          footer.bar,
+          opened && footer.opened,
+          flex.horizontal
+        )}
+      >
+        <IconButton
+          name="search"
+          className={footer.search}
+          onClick={() => setOpened(!opened)}
+        >
+          <Balloon icon="info" delay={5000}>
+            Add charactor to the game
+          </Balloon>
+        </IconButton>
+        {opened ? (
+          <input
+            type="text"
+            autoFocus
+            placeholder="Search monsters, people and items"
+            value={search}
+            onChange={e => setSearch(e.currentTarget.value)}
+          />
+        ) : (
+          <>
+            <FooterItem />
+            <FooterItem />
+            <div className={footer.divider}></div>
+            <FooterItem />
+            <FooterItem />
+            <FooterItem />
+            <FooterItem />
+            <FooterItem />
+            <FooterItem />
+          </>
+        )}
+      </div>
+    </>
+  );
+}
+
+export interface FooterItemProps {}
+
+export function FooterItem({  }: FooterItemProps) {
+  const dispatch = useDispatch();
+  const onClick = React.useCallback(() => {
+    dispatch(actions.setEditorMode(EditorMode.Code));
+  }, []);
+
+  return (
+    <button className={footer.item} onClick={onClick}>
+      <div style={{ backgroundColor: 'blue' }}></div>
+    </button>
+  );
+}
